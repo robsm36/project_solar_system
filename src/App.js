43386074859fa8ddee68a2ed767c50f046eb12f5
Robson Miranda <robsm36@gmail.com>
2022-09-21 21:02:17 -0300

    Requisito 10 - projeto concluido

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './components/Header';
 import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
-// import Title from './components/Title';
 
 class App extends React.Component {
   render() {
@@ -11,7 +10,6 @@ class App extends React.Component {
         <Header />
         <SolarSystem />
         <Missions />
-        {/* <Title headline="oi" /> */}
       </>
     );
   }
